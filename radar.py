4c0bb58363ad1a8f764c5dc3b75cf0a1ff45e4ee
Paul <yqbutcher@hotmail.com>
2024-01-22 14:23:33 +0800

    first commit

---
+++ radar.py
@@ -0,0 +1,163 @@
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.io as pio
+import os
+
+
+def get_radar_dict():
+    """
+    返回包含雷达图配置的字典。
+    """
+    return {'subjects': ['chinese', 'math', 'english', 'physical'],
+            'chinese_columns_1': ['汉语拼音', '识字写字', '阅读理解', '表达交流', '综合实践'],
+            'chinese_columns_2': ['识字写字', '阅读理解', '积累运用', '表达交流', '综合实践'],
+            'math_columns': ['计算能力', '审题能力', '解决问题', '实践操作', '双语能力'],
+            'english_columns': ['Speaking', 'Reading', 'Writing', 'Use of English', 'Listening'],
+            'physical_columns_1': ['BMI', '肺活量', '50米跑', '坐位体前屈', '1分钟跳绳'],
+            'physical_columns_2': ['BMI', '肺活量', '50米跑', '坐位体前屈', '1分钟跳绳', '仰卧卷腹'],
+            }
+
+
+def select_columns_for_subject(sub, grade, radar_dict):
+    """
+    根据学科和年级选择适当地评价维度。
+    """
+    if sub == 'chinese':
+        return radar_dict['chinese_columns_1'] if grade == '1' else radar_dict['chinese_columns_2']
+    elif sub == 'math':
+        return radar_dict['math_columns']
+    elif sub == 'english':
+        return radar_dict['english_columns']
+    elif sub == 'physical':
+        if grade in ['1', '2']:
+            return radar_dict['physical_columns_1']
+        elif grade in ['3', '4']:
+            return radar_dict['physical_columns_2']
+        else:
+            return radar_dict['physical_columns_3']
+
+
+def create_radar_chart(sub, row, select_columns, output_folder):
+    """
+    为每个学生生成雷达图。
+    """
+    select_data = select_columns.copy()
+    select_data.append(select_data[0])
+    values = row[select_data].tolist()
+    theta = select_data
+
+    fig = go.Figure()
+    # 添加雷达图的红色范围
+    fig.add_trace(go.Scatterpolar(
+        r=values,
+        theta=theta,
+        fill='toself',
+        fillcolor='rgba(232, 208, 208, 0.5)',
+        line=dict(width=4, color='#FF5722'),
+        opacity=1,
+        name='',
+    ))
+
+    # 添加圆形标记
+    for i, val in enumerate(values[:-1]):  # 去除起始点
+        fig.add_trace(go.Scatterpolar(
+            r=[val],
+            theta=[theta[i]],
+            mode='markers',  # 设置为圆形节点
+            marker=dict(
+                size=14,  # 圆形节点的大小
+                color='#FF5722',  # 与trace line的颜色一致
+                opacity=1,
+                line=dict(width=0, color='#FF5722'),  # 圆形节点的边界样式
+            ),
+            name='',
+        ))
+
+    vertical_offset = 8  # 自定义垂直偏移量，根据需要调整
+    for i, val in enumerate(values):
+        # 当学科为physical时，跳过不显示分数
+        if sub != 'physical':
+            r = [val - vertical_offset]
+            fig.add_trace(go.Scatterpolar(
+                r=r,
+                theta=[theta[i]],
+                mode='text',
+                text=str(round(val * 0.05, 1)),  # 保留整数部分
+                textposition='bottom center',
+                textfont=dict(family='Microsoft YaHei', size=24, color='#FF5722'),
+                name='',
+            ))
+
+    fig.update_polars(
+        gridshape='circular',  #
+        bgcolor='#ffffff',
+        radialaxis=dict(visible=True,
+                        showgrid=True,
+                        showticklabels=False,
+                        gridwidth=2,
+                        gridcolor='rgba(128, 128, 128, 0.2)',
+                        linewidth=0,
+                        range=[0, 100],
+                        tickvals=[25, 50, 75, 100],  # 设置刻度值
+                        nticks=4,  # 设置刻度线数量
+                        ),
+        angularaxis=dict(showgrid=True,  # 角度轴网格线
+                         linecolor='rgba(128, 128, 128, 0.2)',
+                         linewidth=2,
+                         gridwidth=2,
+                         gridcolor='rgba(128, 128, 128, 0.4)',
+                         direction="clockwise",  # 设置方向为顺时针，使底边水平
+                         tickfont=dict(family='Microsoft YaHei', size=50, color='black'),
+                         ),
+    )
+
+    fig.update_layout(
+        showlegend=False,  # 不显示图例
+        # title=f'{sub}',
+        # title_x=0.05,
+        # title_y=0.95,
+        # title_font=dict(family='Arial Bold', size=60, color='black'),
+        height=900,
+        width=1200,
+    )
+
+    # 保存雷达图为.png文件
+    student_name = row['学生姓名']
+    output_file = os.path.join(output_folder, f"{student_name}.png")
+    pio.write_image(fig, output_file, format="png")
+    print(f"Saved {sub} {output_file}!")
+
+
+def get_grade_and_folder_path():
+    while True:
+        grade = input('请输入要操作的年级数字（如1、2、3）：')
+        if grade.isdigit() and 1 <= int(grade) <= 6:
+            folder_path = os.path.join(os.path.dirname(__file__), f'Y{grade}_RADAR')
+            if not os.path.exists(folder_path):
+                print(f'找不到文件目录: {folder_path}')
+                continue
+            break
+        else:
+            print("输入的数字不符合要求，请输入小于或等于6的数字。")
+    return grade, folder_path
+
+
+def main():
+    radar_dict = get_radar_dict()
+    grade, folder_path = get_grade_and_folder_path()
+    # 读取xlsx文件
+    xlsx_file = os.path.join(folder_path, 'report.xlsx')
+    df = pd.read_excel(xlsx_file)
+
+    # 创建文件夹以保存雷达图
+    for sub in radar_dict['subjects']:
+        output_folder = os.path.join(folder_path, sub)
+        os.makedirs(output_folder, exist_ok=True)
+
+        for _, row in df.iterrows():
+            select_columns = select_columns_for_subject(sub, grade, radar_dict)
+            create_radar_chart(sub, row, select_columns, output_folder)
+
+
+if __name__ == '__main__':
+    main()
